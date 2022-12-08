2682c91ef825281d16b00757348fb05e934497f2
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-12-08 13:34:03 +0300

    creating vm

--- 1. IAAS/createVM.sh
+++ 1. IAAS/createVM.sh
@@ -6,3 +6,14 @@
 az group list --output table
 # 2. Gerekirse yeni bir resource-group oluştur:
 az group create --name bademo-vm-cli-rg --location "westeurope"
+
+az vm create \
+            --resource-group bademo-vm-cli-rg \
+            --name "bademo-win-cli" \
+            --image "win2019datacenter" \
+            --admin-username "turkay" \
+            --admin-password "Password1234"
+
+
+
+        
