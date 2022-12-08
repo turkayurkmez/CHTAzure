8e83f3a0d0d149b4677ea1d3db3630c0e77af316
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-12-08 13:39:55 +0300

    open RDP port

--- 1. IAAS/createVM.sh
+++ 1. IAAS/createVM.sh
@@ -6,14 +6,18 @@
 az group list --output table
 # 2. Gerekirse yeni bir resource-group oluştur:
 az group create --name bademo-vm-cli-rg --location "westeurope"
-
+# 3. Temel parametreler ile sanal makine oluştur.
 az vm create \
             --resource-group bademo-vm-cli-rg \
             --name "bademo-win-cli" \
             --image "win2019datacenter" \
             --admin-username "turkay" \
             --admin-password "Password1234"
-
+# 4. RDP Port'unu aç
+az vm open-port \
+            --resource-group bademo-vm-cli-rg \
+            --name "bademo-win-cli"  \
+            --port "3389" \
 
 
         
